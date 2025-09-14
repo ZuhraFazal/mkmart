0c0a25beac02af99c658ddb854a85b18588eba35
ZuhraFazal <zuhrafazal0@gmail.com>
2025-09-14 13:01:51 +0500

    url

--- products/urls.py
+++ products/urls.py
@@ -8,3 +8,8 @@ urlpatterns = [
     path("", views.product_list, name="product_list"),
     path("<int:pk>/", views.product_detail, name="product_detail"),   # yaha pk hai
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
