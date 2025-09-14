3ec24df1ee0dfa5096aa08babdd208cd3f8ffcb3
ZuhraFazal <zuhrafazal0@gmail.com>
2025-09-14 11:40:58 +0500

    new changes

--- mk_mart/settings.py
+++ mk_mart/settings.py
@@ -130,12 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
