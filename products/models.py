4f5bab502eaa08a1751f158f76da16f936cc69d6
ZuhraFazal <zuhrafazal0@gmail.com>
2025-09-14 16:45:45 +0500

    product card - navigation chnages

--- products/models.py
+++ products/models.py
@@ -11,7 +11,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    # image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
     stock = models.PositiveIntegerField(default=10)
 
     def __str__(self):
